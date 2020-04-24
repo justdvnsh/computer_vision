6adbd6f38ec5e3f54624d6cd1c52c897ad0f8ab8
justdvnsh <justdvnsh2208@gmail.com>
2020-04-24 17:05:34 +0530

    added files

--- face_detection.py
+++ face_detection.py
@@ -5,21 +5,37 @@ file_path_eyes = 'haarcascade_eye.xml'
 faceCascade = cv2.CascadeClassifier(file_path_face)
 eyeCascade = cv2.CascadeClassifier(file_path_eyes)
 
-cap = cv2.VideoCapture(0)
+# parameters
+cap_region_x_begin = 0.5  # start point/total width
+cap_region_y_end = 0.8  # start point/total width
 
+cap = cv2.VideoCapture(0)
+cap.set(10,200)
+
+## Place your head in the blue box to detect faces.
+## change the img to frame to detect the faces in the complete frame
 while(True):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    faces = faceCascade.detectMultiScale(frame, 1.3, 5)
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+
+    img = frame[0:int(cap_region_y_end * frame.shape[0]),
+          int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]
+
+    faces = faceCascade.detectMultiScale(img, 1.3, 5)
     for x,y,w,h in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 6)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 6)
         eyes = eyeCascade.detectMultiScale(gray)
         for (ex, ey,ew,eh) in eyes:
-            image = cv2.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 6)
+            cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 6)
 
+    frame = cv2.flip(frame, 1)
+    # cv2.rectangle(frame, (300, 100), (500, 400), (0,255,0), 6)
     cv2.imshow('frame', frame)
+    cv2.imshow('img', img)
     # cv2.imshow('gray', gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
